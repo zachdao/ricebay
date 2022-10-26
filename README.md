6976fd4bab98d310c9b686a7cfaf642b8ac2b446
Dustin Homan <deh5@rice.edu>
2022-10-25 20:35:48 -0600

    feat: add schema migration script and initial schema
    A common rough spot when multiple people are developing against the DB is for someone to change the schema, have it work locally, but not work on other developer boxes or in prod. An easy way to share these transitions from one schema version to the next is with a migrator. A lot of ORMs have them baked in. Since we aren't using an ORM, this utility provides just that.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 Team name: `COMP610 RiceBay Team B`
 
-Heroku app URL: `https://comp610-ricebay-team-b.herokuapp.com/`
+Heroku app URL: [`https://comp610-ricebay-team-b.herokuapp.com/`](https://comp610-ricebay-team-b.herokuapp.com/)
 
 ## Dependencies
 ### Java SDK 17+
@@ -47,7 +47,6 @@ To open your app in a local browser, navigate to http://localhost:4567 or your d
  # Will not succeed if your code contains any error.
 ```
 
-
 ## How to test
 
 ### Test in IDEA
@@ -63,3 +62,58 @@ Execute following:
 ```shell
 mvn clean test
 ```
+
+## Development Guides
+
+### Adding a new table? Altering a table? You need the "migrator"!
+
+To control our database schema changes, we have a small python utility called the migrator. A migration consists of an
+"up" action, that is the steps needed to migrate the database from one state to the desired next state. And a "down" action,
+that is the steps needed to undo the "up" action. The script tracks which migrations have been run against the target DB and
+only runs the ones it needs to.
+
+#### Generate a migration
+
+To generate a new migration, pick a useful migration name (i.e. InitialSchema or RemoveDefaultPayment) that is unique to
+the migration, and then run:
+
+```shell
+cd database
+python migrator.py generate <migration name>
+```
+
+The new migration class will be in `database/migrations/<migration name>.py` with empty "up" and "down" functions. As an
+argument to the "up" and "down" functions, the migrator will pass you a psycopg2 cursor so that you can execute your
+desired SQL.
+
+As displayed in the output, please add an import in `database/migrator.py` for your new migration. We need to have the
+import so that the decorators that register the migrations are executed.
+
+#### Running Migrations
+
+To apply the migrations in the `migrations` directory, get your database connection information and run:
+
+```shell
+cd database
+python migrator.py migrate --db-host <hostname> --db-port <port> --db-user <username> --db-password <password> --db-name <database name> up
+```
+
+This will inspect the `migrations` table in the target database (or create one if it doesn't exist) and run all migrations
+that haven't been run against the target database.
+
+#### Need to undo a migration?
+
+To apply the "down" function to a database, find out which migrations the database has run by running:
+
+```shell
+cd database
+python migrator.py migrate --db-host <hostname> --db-port <port> --db-user <username> --db-password <password> --db-name <database name> list
+```
+
+Determine the state that you wish to revert to. Also read as, what is the last migration you want applied? Now run the following:
+
+```shell
+python migrator.py migrate --db-host <hostname> --db-port <port> --db-user <username> --db-password <password> --db-name <database name> list --stop-at <last migration name>
+```
+
+Is your desired state a clean DB? If so, just omit the `--stop-at` argument.
---
+++ database/migrations/InitialSchema.py
@@ -0,0 +1,150 @@
+from database.migration import migration
+
+
+@migration(order=1)
+class InitialSchema:
+
+    def up(self, db_cursor):
+        print(f'Creating initial schema')
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS account (
+    id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+    alias varchar NOT NULL UNIQUE,
+    email varchar NOT NULL UNIQUE,
+    given_name varchar DEFAULT '' NOT NULL,
+    surname varchar DEFAULT '' NOT NULL,
+    password varchar NOT NULL,
+    last_password varchar,
+    image bytea,
+    default_payment_method int
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS payment_method (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    zelle_id varchar NOT NULL UNIQUE,
+    owner_id uuid NOT NULL
+        CONSTRAINT owner
+            REFERENCES account
+            ON DELETE CASCADE,
+    DEFAULT_method boolean DEFAULT false
+);
+        """)
+        db_cursor.execute("""
+ALTER TABLE account ADD
+    CONSTRAINT payment
+        FOREIGN KEY (default_payment_method) REFERENCES payment_method (id)
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS rating (
+    rater_id uuid NOT NULL
+        CONSTRAINT rater
+            REFERENCES account
+            ON DELETE CASCADE,
+    seller_id uuid NOT NULL
+        CONSTRAINT seller
+            REFERENCES account
+            ON DELETE CASCADE,
+    rating smallint NOT NULL
+        CONSTRAINT rating_rating_check
+            CHECK (rating <= 5),
+    PRIMARY KEY (rater_id, seller_id)
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS auction (
+    id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+    title varchar NOT NULL,
+    description varchar NOT NULL,
+    minimum_bid integer NOT NULL,
+    bid_increment integer
+        CONSTRAINT auction_bid_increment_check
+            CHECK (bid_increment > 0),
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    tax_percent double precision,
+    state integer NOT NULL,
+    owner_id uuid NOT NULL
+        CONSTRAINT owner
+            REFERENCES account
+            ON DELETE CASCADE,
+    CONSTRAINT end_after_start
+        CHECK (start_date > end_date)
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS auction_view (
+    id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+    timestamp date DEFAULT now(),
+    auction_id uuid NOT NULL
+        CONSTRAINT auction
+            REFERENCES auction
+            ON DELETE CASCADE,
+    viewer_id uuid NOT NULL
+        CONSTRAINT viewer
+            REFERENCES account
+            ON DELETE CASCADE
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS category (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar NOT NULL UNIQUE,
+    description varchar
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS picture (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    picture_name varchar NOT NULL,
+    picture_sequence smallint DEFAULT 0,
+    picture_data bytea NOT NULL,
+    auction_id uuid NOT NULL
+        CONSTRAINT auction
+            REFERENCES auction
+            ON DELETE CASCADE
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS bid (
+    id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+    timestamp date DEFAULT now(),
+    amount integer NOT NULL
+        CONSTRAINT bid_amount_check
+            CHECK (amount > 0),
+    owner_id uuid NOT NULL
+        CONSTRAINT bidder
+            REFERENCES account
+            ON DELETE CASCADE,
+    auction_id uuid NOT NULL
+        CONSTRAINT auction
+            REFERENCES auction
+            ON DELETE CASCADE
+);
+        """)
+        db_cursor.execute("""
+CREATE TABLE IF NOT EXISTS auction_category (
+    auction_id uuid NOT NULL
+        CONSTRAINT auction
+            REFERENCES auction
+            ON DELETE CASCADE,
+    category_id integer NOT NULL
+        CONSTRAINT category
+            REFERENCES category
+            ON DELETE CASCADE,
+    PRIMARY KEY (auction_id, category_id)
+);
+        """)
+        print(f'Finished creating needed tables')
+
+    def down(self, db_cursor):
+        db_cursor.execute("DROP TABLE IF EXISTS auction_category;")
+        db_cursor.execute("DROP TABLE IF EXISTS picture;")
+        db_cursor.execute("DROP TABLE IF EXISTS auction_view;")
+        db_cursor.execute("DROP TABLE IF EXISTS bid;")
+        db_cursor.execute("DROP TABLE IF EXISTS rating;")
+        db_cursor.execute("DROP TABLE IF EXISTS category;")
+        db_cursor.execute("DROP TABLE IF EXISTS auction;")
+        db_cursor.execute("ALTER TABLE account DROP CONSTRAINT payment;")
+        db_cursor.execute("DROP TABLE IF EXISTS payment_method;")
+        db_cursor.execute("DROP TABLE IF EXISTS account;")
