0d402b5fa21773a87d7dd02777af9f998c2a8b8b
Dustin Homan <deh5@rice.edu>
2022-11-08 23:18:26 -0700

    chore: add comment explaining what routes component does

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -12,23 +12,29 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <Provider theme={defaultTheme}>
+        {/* set up routing based on URL path */}
         <BrowserRouter>
+            {/* switch on path */}
             <Routes>
+                {/* path = / */}
                 <Route
                     index
                     element={<App />}
                     errorElement={<NotFoundError />}
                 />
+                {/* path = /login */}
                 <Route
                     path="login"
                     element={<Login />}
                     errorElement={<NotFoundError />}
                 />
+                {/* path = /register */}
                 <Route
                     path="register"
                     element={<Register />}
                     errorElement={<NotFoundError />}
                 />
+                {/* path = * */}
                 <Route
                     path="*"
                     element={<App />}
