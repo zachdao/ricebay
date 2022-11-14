3bba40ea82451f05e494d4278381d51d9438ac41
zachdao <zrd3@rice.edu>
2022-11-13 18:02:55 -0600

    Work in Progress of the front end to place a bid on an item.

---
+++ src/frontend/src/auction-bid/AuctionBid.jsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Flex,
+    Grid,
+    Heading,
+    Text,
+    View,
+    Button,
+    Dialog,
+    DialogTrigger,
+    Content,
+    TextField,
+} from '@adobe/react-spectrum';
+import { useParams } from 'react-router-dom';
+import { useHttpQuery } from '../http-query/use-http-query';
+
+export const AuctionBid = () => {
+    const auction = {
+        title: 'Obnoxiously Long Title Blah Blah Blah',
+        minimumBid: 20.0,
+        published: 'Published',
+        ownerId: 12345,
+        categoryIds: [1, 2, 3],
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
+            'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
+            'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore ' +
+            'eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' +
+            'mollit anim id est laborum.',
+        seller: 'coolguy25',
+        images: [],
+        maxBid: 500.0,
+    };
+
+    // Get our single auction listing, where appResponse is AppResponse<Auction>
+    // const { auctionId } = useParams();
+    // const { appResponse, error, status } = useHttpQuery(
+    //     `/auctions/${auctionId}`,
+    // );
+    // const auction = appResponse?.data;
+
+    // Display a toast if we have an error
+    // useEffect(() => {
+    //     if (
+    //         error ||
+    //         (status && status !== 200) ||
+    //         (appResponse && !appResponse.success)
+    //     ) {
+    //         toast.custom((t) => (
+    //             <Toast
+    //                 message={
+    //                     appResponse?.msg ||
+    //                     'Failed to find auction! Try again later'
+    //                 }
+    //                 type="negative"
+    //                 dismissFn={() => toast.remove(t.id)}
+    //             />
+    //         ));
+    //     }
+    // }, [appResponse, error, status]);
+
+    // Set up text field data
+    const [yourBid, makeBid] = useState('');
+    const [maxBid, changeMaxBid] = useState('');
+    const [error, setError] = useState('');
+
+    // TODO Once the backend sends all the necessary data, map the fields to the (currently) missing variables
+    // TODO Handle bad calls to make the bid
+    // TODO Handle bad calls to increase max bid
+    // TODO Disable button if user currently has highest bid
+    // TODO re-enable button and give notification if they have been outbid
+
+    // Render data if found
+    return (
+        <Grid
+            margin="0 auto"
+            marginTop="size-400"
+            width="50%"
+            areas={['image details', 'description description']}
+            columns={['3fr', '2fr']}
+            rows={['50%', '50%']}
+            height="90%"
+            gap="size-250"
+        >
+            <View gridArea="image">
+                {auction.images.length ? (
+                    <img src={auction.images[0]} alt="image of auction item" />
+                ) : (
+                    <View
+                        width="100%"
+                        height="100%"
+                        backgroundColor="gray-800"
+                    ></View>
+                )}
+            </View>
+
+            <Flex direction="column" gridArea="details">
+                <Heading alignSelf="center" level={1}>
+                    {auction.title}
+                </Heading>
+                <Grid
+                    areas={[
+                        'currentbid bidvar',
+                        'status statusvar',
+                        'seller sellervar',
+                        'sellerrating ratingvar',
+                        'categories catvar',
+                    ]}
+                    columns={['2fr', '3fr']}
+                    rows={['1fr', '1fr', '1fr', '1fr', '1fr']}
+                    height="100%"
+                    justifyContent="space-evenly"
+                >
+                    <Text gridArea="currentbid"> Current Bid:</Text>
+                    <Text gridArea="bidvar">{auction.minimumBid}</Text>
+                    <Text gridArea="status">Status:</Text>
+                    <Text gridArea="statusvar">{auction.published}</Text>
+                    <Text gridArea="seller">Seller:</Text>
+                    <Text gridArea="sellervar">{auction.seller}</Text>
+                    <Text gridArea="sellerrating">Seller Rating:</Text>
+                    <Text gridArea="ratingvar">
+                        {auction.sellerRating} star(s)
+                    </Text>
+                    <Text gridArea="categories">Categories:</Text>
+                    <Text gridArea="catvar">{auction.categoryIds}</Text>
+                </Grid>
+                <DialogTrigger type="popover">
+                    <Button alignSelf="center" variant="cta">
+                        Increase Bid (Max: ${auction.maxBid})
+                    </Button>
+                    <Dialog>
+                        <Heading>Place your bid</Heading>
+                        <Content>
+                            <Flex
+                                direction="rows"
+                                justifyContent="space-evenly"
+                                alignContent="center"
+                                marginTop="size-100"
+                                gap="size-100"
+                            >
+                                <TextField
+                                    validationState={
+                                        error ? 'invalid' : undefined
+                                    }
+                                    label="Your bid"
+                                    value={yourBid}
+                                    onChange={makeBid}
+                                    type="double"
+                                    onBlur={() => setError('')}
+                                />
+                                <TextField
+                                    validationState={
+                                        error ? 'invalid' : undefined
+                                    }
+                                    label="Max bid"
+                                    value={maxBid}
+                                    onChange={changeMaxBid}
+                                    type="double"
+                                    onBlur={() => setError('')}
+                                />
+                                <Button alignSelf="center" variant="cta">
+                                    Make bid
+                                </Button>
+                            </Flex>
+                        </Content>
+                    </Dialog>
+                </DialogTrigger>
+            </Flex>
+
+            <Flex
+                direction="column"
+                space="size-50"
+                width="100%"
+                alignItems="start"
+                gridArea="description"
+                borderWidth="thin"
+                borderColor="dark"
+            >
+                <Heading level={3}>Description</Heading>
+                <Text>{auction.description}</Text>
+            </Flex>
+        </Grid>
+    );
+};
