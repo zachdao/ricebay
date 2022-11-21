897b3b56a8071263dd30575c2e15384d3fb222ad
Dustin Homan <dustin.spartan.coding@gmail.com>
2022-11-20 21:56:49 -0700

    chore: update tests to match last commits changes

--- src/frontend/src/auction/auction-view/AuctionView.spec.jsx
+++ src/frontend/src/auction/auction-view/AuctionView.spec.jsx
@@ -1,52 +1,51 @@
 import React from 'react';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
 import { AuctionView } from './AuctionView';
-import { MemoryRouter } from 'react-router-dom';
 import { defaultTheme, Provider } from '@adobe/react-spectrum';
-import userEvent from '@testing-library/user-event';
 import mockAxios from 'jest-mock-axios';
+import {DateTime} from "luxon";
+import {MemoryRouter} from "react-router-dom";
+
+const auction = {
+    title: 'Obnoxiously Long Title Blah Blah Blah',
+    minimumBid: 20.0,
+    published: 'Published',
+    categories: ['Furniture'],
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
+        'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
+        'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore ' +
+        'eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' +
+        'mollit anim id est laborum.',
+    seller: {
+        id: 12345,
+        alias: 'coolguy25',
+        rating: 3,
+    },
+    images: [],
+    bidIncrement: 2.0,
+    startDate: DateTime.now().minus({days: 1}).toISODate(),
+    endDate: DateTime.now().plus({days: 1}).toISODate(),
+};
 
 const renderComponent = async () => {
     await render(
         <Provider theme={defaultTheme}>
-            <AuctionView />
+            <MemoryRouter>
+                <AuctionView auction={auction} />
+            </MemoryRouter>
         </Provider>,
     );
 };
 
-describe('AuctionBid', () => {
+describe('AuctionView', () => {
     afterEach(() => {
         mockAxios.reset();
     });
 
-    it(
-        'should render text fields for title, current bid, minimum bid increment, status, seller, seller ' +
-            'rating, categories, and description. should render image. should render button to increase bid and ' +
-            'popover to place bid',
-        async () => {
-            const auction = {
-                title: 'Obnoxiously Long Title Blah Blah Blah',
-                minimumBid: 20.0,
-                published: 'Published',
-                ownerId: 12345,
-                categoryIds: [1, 2, 3],
-                description:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
-                    'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
-                    'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore ' +
-                    'eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' +
-                    'mollit anim id est laborum.',
-                seller: 'coolguy25',
-                images: [],
-                maxBid: 500.0,
-                bidIncrement: 2.0,
-            };
-
+    it('should render the auction details', async () => {
             await renderComponent();
-
-            expect(
-                screen.queryByDisplayValue(auction.title).toBeInTheDocument(),
-            );
+            expect(screen.getByText(auction.title)).toBeInTheDocument();
         },
     );
 
--- src/frontend/src/auction/edit-auction/EditAuction.spec.jsx
+++ src/frontend/src/auction/edit-auction/EditAuction.spec.jsx
@@ -5,92 +5,93 @@ import { MemoryRouter } from 'react-router-dom';
 import { defaultTheme, Provider } from '@adobe/react-spectrum';
 import userEvent from '@testing-library/user-event';
 import mockAxios from 'jest-mock-axios';
+import {DateTime} from "luxon";
+
+const auction = {
+    title: 'Obnoxiously Long Title Blah Blah Blah',
+    minimumBid: 20.0,
+    published: 'Published',
+    categories: ['Furniture'],
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
+        'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
+        'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore ' +
+        'eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' +
+        'mollit anim id est laborum.',
+    seller: {
+        id: 12345,
+        alias: 'coolguy25',
+        rating: 3,
+    },
+    images: [],
+    bidIncrement: 2.0,
+    startDate: DateTime.now().minus({days: 1}).toISODate(),
+    endDate: DateTime.now().plus({days: 1}).toISODate(),
+};
 
 const renderComponent = async () => {
     await render(
         <Provider theme={defaultTheme}>
             <MemoryRouter>
-                <EditAuction />
+                <EditAuction auction={auction} />
             </MemoryRouter>
         </Provider>,
     );
 };
 
-describe('Create Auction', () => {
+describe('Edit Auction', () => {
     afterEach(() => {
         mockAxios.reset();
     });
 
-    it(
-        'should render text inputs for title, starting bid, bid increment, and description.' +
-            'should render buttons for publishing, cancelling, saving, and uploading image. ' +
-            'should render fields for adding categories, categories, start and end dates',
-        async () => {
+    it('should render a form to update auctions', async () => {
             await renderComponent();
 
             expect(
-                screen.queryByLabelText('Auction Title'),
+                screen.getByLabelText('Auction Title'),
             ).toBeInTheDocument();
-            expect(screen.queryByLabelText('Starting Bid')).toBeInTheDocument();
+            expect(screen.getByLabelText(/Starting Bid.*/)).toBeInTheDocument();
             expect(
-                screen.queryByLabelText('Minimum Bid Increment'),
+                screen.getByLabelText('Bid Increment'),
             ).toBeInTheDocument();
             expect(
-                screen.queryByLabelText('Auction Description'),
+                screen.getByLabelText('Auction Description'),
             ).toBeInTheDocument();
 
-            expect(screen.queryByText('Publish')).toBeInTheDocument();
-            expect(screen.queryByText('Cancel')).toBeInTheDocument();
-            expect(screen.queryByText('Save')).toBeInTheDocument();
-            expect(screen.queryByText('Upload Image')).toBeInTheDocument();
+            expect(screen.getByText('Publish')).toBeInTheDocument();
+            expect(screen.getByText('Cancel')).toBeInTheDocument();
+            expect(screen.getByText('Save')).toBeInTheDocument();
+            expect(screen.getByText('Upload Image')).toBeInTheDocument();
 
-            expect(screen.queryByText('Add Category')).toBeInTheDocument();
-            expect(screen.queryByText('Categories')).toBeInTheDocument();
-            expect(screen.queryByText('Start Date')).toBeInTheDocument();
-            expect(screen.queryByText('End Date')).toBeInTheDocument();
+            expect(screen.getByText('Add Category')).toBeInTheDocument();
+            expect(screen.getByText('Categories')).toBeInTheDocument();
+            expect(screen.getByText(/Auction Dates.*/)).toBeInTheDocument();
         },
     );
 
-    it('should call /auctions with field values when publish is clicked', async () => {
+    it('should save the auction with field values when saved is clicked', async () => {
         const auction = userEvent.setup();
 
         await renderComponent();
 
         const title = 'title';
-        const category = 'category';
-        const startDate = '1/1/2023';
-        const endDate = '1/2/2023';
         const startingBid = '20.0';
         const bidIncrement = '2.0';
         const description = 'description';
-        const image = '';
 
         await auction.type(screen.getByLabelText('Auction Title'), title);
-        await auction.type(screen.getByLabelText('Starting Bid'), startingBid);
+        await auction.type(screen.getByLabelText(/Starting Bid.*/), startingBid);
         await auction.type(
-            screen.getByLabelText('Minimum Bid Increment'),
+            screen.getByLabelText('Bid Increment'),
             bidIncrement,
         );
         await auction.type(
             screen.getByLabelText('Auction Description'),
             description,
         );
-        await auction.type(screen.getByText('Upload Image'), image);
-        await auction.type(screen.getByText('Categories'), category);
-        await auction.type(screen.getByText('Start Date'), startDate);
-        await auction.type(screen.getByText('End Date'), endDate);
 
-        await auction.click(screen.getByText('Publish'));
+        await auction.click(screen.getByText('Save'));
 
-        expect(mockAxios.post).toHaveBeenCalledWith('/auctions', {
-            title,
-            image,
-            startingBid,
-            bidIncrement,
-            startDate,
-            endDate,
-            category,
-            description,
-        });
+        expect(mockAxios.post).toHaveBeenCalledWith(`/auctions/${auction.id}`, expect.anything());
     });
 });
